8ad7622c22cc076e8bdb2a388bb0a1b6b2d3f953
ivanozil11 <ivancalfer1@gmail.com>
2019-03-12 18:50:23 +0100

    Commit Final

--- JuegoAhorcado.java
+++ JuegoAhorcado.java
@@ -3,57 +3,89 @@ import java.util.Scanner;
 
 public class JuegoAhorcado {
 
-    /**
-     * Constructor for objects of class JuegoAhorcado
-     */
+    static final int BONUS = 6;
+
     public JuegoAhorcado() {
 
     }
 
+
     public void iniciarJuego() {
-        boolean adivinada = false;
-        int puntuacion = 1000;
+        boolean finalJuego = false;
+        int vida = 6;
 
         Scanner sc = new Scanner(System.in);
         String[] palabras = {"Armario", "Mueble", "Radiador", "Estufa", "Enchufe", "Mesa", "Ordenador", "Radiocasette", "Libro", "Madrid"};
         Random r = new Random();
         int indicePalabra = r.nextInt(palabras.length);
         String palabraElegida = palabras[indicePalabra];
-        //Hasta aqui el juego elige la palabra a adivinar
+        int tamaño = palabraElegida.length();
+        char[] palabraFinal = new char[tamaño];
+        for (int i = 0; i < tamaño; i++) {
+            palabraFinal[i] = '*';
+        }
+        //Hasta aqui el programa se enccarga de generar un una palabra escogida aleatoriamente de las del array
+        //a su vez, se genera un array de elementos char, con el tamaño exactoque de caracteresque lapalabra esogida
+        //y los rellena con asteriscos. Ahora entramos en el juego
 
-        while (adivinada == false) {
-            System.out.println("Introduce una letra (1) o intenta probar suerte (2)");
+        while (finalJuego == false || vida == 0) {//Se nos permite elegir si introducir una letra ointentar adivinar la palabra
+            System.out.println("\nIntroduce una letra (1) o intenta probar suerte (2)");
             int eleccion = sc.nextInt();
             if (eleccion == 1) {
-
+                // introducimos una letra mediante el escaner
                 System.out.println("Introduce la letra");
                 String letra = sc.next();
-                if (!(letra.length() == 1)) {
-                    System.out.println("Error, solo una letra");
-                    letra = sc.nextLine();
-                } else {
-                    if (palabraElegida.contains(letra)) {
-                        System.out.println("Correcto, la letra " + letra + " esta en la palabra elegida");
-                    } else {
-                        System.out.println("MUAHAHAHAHA, ESA LETRA NO ESTA");
-                        puntuacion =puntuacion -10;
+                
+                //El programa comparala letracon la palabra y si la palabra contiene la letra elegida, 
+                //se muestra por pantalla la palabra cubierta por asteriscos, sustituidos por la o las letras correctas;
+                if (palabraElegida.contains(letra)) {
+
+                    System.out.println("Correcto, esta letra esta en la palabra");
+
+                    for (int i = 0; i < tamaño; i++) {//se sustituye en el array de char, las letras que coinciden en la palabra
+                        if (palabraElegida.charAt(i) == letra.charAt(0)) {
+                            palabraFinal[i] = letra.charAt(0);
+                        }
+                    }
+                    for (int i = 0; i < tamaño; i++) {
+                        System.out.print("__" + palabraFinal[i] + "__  ");
+                    }
+                } else {//En caso de que no exista la letra elegida en la palabra ,se pierde una vida
+                    
+                    vida--;
+                    System.out.println("Te quedan " + vida + " vidas");
+                    if (vida == 0) {//En caso de llegar a 0,se termina eljuego con derrota
+                        System.out.println("HAS PERDIDO, SE ACABO EL JUEGO");
                     }
                 }
 
             }
-            if (eleccion == 2) {
+            if (eleccion == 2) {//ya sabemos la palabra y queremos acetarla y ganar el bonus
                 System.out.println("Introduce la palabra, si fallas perderas vida");
                 String palabra = sc.next();
+                //si la palabra es correcta , nos muestra un mensaje de victoria por pantalla
+                //junto con la puntuacion del jugador (la vida con la que termina + el bonus)
                 if (palabra.equals(palabraElegida.toLowerCase())) {
+                    
+                    vida = vida + BONUS;
                     System.out.println("ENHORABUENA,HAS ACERTADO LA PALABRA,HAS GANADO");
-                    System.out.println("TU PUNTUACION ES " + puntuacion);
-                    adivinada=true;
-                }
-                else{
-                    System.out.println("Error");
-                    puntuacion = puntuacion-10;
+                    System.out.println("TU PUNTUACION ES " + vida);
+                    
+
+                    //si es incorrecta, seresta vida y se continua jugando hasta que llega a 0
+                    
+                } else {
+                    
+                    System.out.println("Has fallado,perderás vida");
+                    vida = vida - 1;
+                    //si llega a 0 la vida, el juego se termina con derrota
+                    if (vida == 0) {
+                        finalJuego = true;
+                        System.out.println("SE ACABÓ EL JUEGO, HAS PERDIDO");
+                    }
                 }
             }
         }
+
     }
 }
